f63571668d0f5244669e5f19dbe4e3093e3f5a15
shanhinpavel-2019 <47977289+shanhinpavel-2019@users.noreply.github.com>
2019-03-21 20:03:24 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ // your solution
+ let arrayBrackets = [];
+ let count = 0;
+
+ while(str[count]) {
+  bracketsConfig.forEach((element) => {
+    let lastElement = arrayBrackets[arrayBrackets.length - 1];
+    if (str[count] === element[0]) {
+      if (element[0] === element[1] && lastElement === str[count]) {
+        arrayBrackets.pop(lastElement);
+      } else arrayBrackets.push(str[count]);
+        } else if (lastElement === element[0] && str[count] === element[1]) {
+          arrayBrackets.pop(lastElement);
+        }
+  });
+  count++;
+}
+return arrayBrackets.length === 0 ? true : false;
 }
